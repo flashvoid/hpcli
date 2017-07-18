8578dc76f286c0b6d36d04642defff6383041f7e
Stas A. Kraev <mail@kraev.me>
2017-07-18 23:30:40 +1200

    Init

---
+++ gattt.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+
+	log "github.com/romana/rlog"
+)
+
+var (
+	CommandCodes = map[string]string{
+		"write": "00",
+		"read":  "01",
+		"reset": "02", // "020000",
+		"save":  "03", // "030000",
+	}
+)
+
+var (
+	OpCodes = map[string]string{
+		"curr-time":       "00",
+		"dry-time":        "01",
+		"water-time":      "02",
+		"day-time":        "03",
+		"night-time":      "04",
+		"last-water-time": "05",
+		"white-level":     "06",
+		"red-level":       "07",
+		"green-level":     "08",
+		"blue-level":      "09",
+	}
+
+	OpLength = map[string]string{
+		"":                "00",
+		"curr-time":       "04",
+		"dry-time":        "04",
+		"water-time":      "04",
+		"day-time":        "04",
+		"night-time":      "04",
+		"last-water-time": "04",
+		"white-level":     "01",
+		"red-level":       "01",
+		"green-level":     "01",
+		"blue-level":      "01",
+	}
+)
+
+func getMapKeys(m map[string]string) string {
+	var res string
+	for k, _ := range m {
+		res += fmt.Sprintf("|%s", k)
+	}
+	return res
+}
+
+const (
+	BtListen = "--listen"
+)
+
+type Gattt struct {
+	Interface  string
+	Handle     string
+	Command    string
+	Op1        string
+	Length     string
+	Op2        string
+	GatttolBin string
+	Mac        string
+}
+
+func (g Gattt) Write() error {
+	args := []string{
+		"-i", g.Interface,
+		fmt.Sprintf("--device=%s", g.Mac),
+		"--char-write-req",
+		fmt.Sprintf("--handle=%s", g.Handle),
+		fmt.Sprintf("--value=%s%s%s%s", g.Command, g.Op1, g.Length, g.Op2), // "010004"),
+	}
+
+	log.Infof("Executing %s %v", g.GatttolBin, args)
+
+	cmd := exec.Command(g.GatttolBin, args...)
+
+	_, err := TimedGatttExec(cmd, GatttTimeout, GatttNotificationRead)
+	return err
+}
+
+func (g Gattt) Read() ([]string, error) {
+	args := []string{
+		"-i", g.Interface,
+		fmt.Sprintf("--device=%s", g.Mac),
+		"--char-write-req",
+		fmt.Sprintf("--handle=%s", g.Handle),
+		fmt.Sprintf("--value=%s%s%s%s", g.Command, g.Op1, g.Length, g.Op2),
+		BtListen}
+
+	log.Infof("Executing %s %v", g.GatttolBin, args)
+
+	cmd := exec.Command(g.GatttolBin, args...)
+
+	res, err := TimedGatttExec(cmd, GatttTimeout, GatttNotificationRead)
+	return res, err
+}
+
+func (g Gattt) Save() error {
+	args := []string{
+		"-i", g.Interface,
+		fmt.Sprintf("--device=%s", g.Mac),
+		"--char-write-req",
+		fmt.Sprintf("--handle=%s", g.Handle),
+		fmt.Sprintf("--value=%s%s%s%s", g.Command, g.Op1, g.Length, g.Op2), // "010004"),
+	}
+
+	log.Infof("Executing %s %v", g.GatttolBin, args)
+
+	cmd := exec.Command(g.GatttolBin, args...)
+
+	_, err := TimedGatttExec(cmd, GatttTimeout, GatttNotificationRead)
+	return err
+}
+
+func (g Gattt) Reset() error {
+	args := []string{
+		"-i", g.Interface,
+		fmt.Sprintf("--device=%s", g.Mac),
+		"--char-write-req",
+		fmt.Sprintf("--handle=%s", g.Handle),
+		fmt.Sprintf("--value=%s%s%s%s", g.Command, g.Op1, g.Length, g.Op2), // "010004"),
+	}
+
+	log.Infof("Executing %s %v", g.GatttolBin, args)
+
+	cmd := exec.Command(g.GatttolBin, args...)
+
+	_, err := TimedGatttExec(cmd, GatttTimeout, GatttNotificationRead)
+	return err
+}
+
+var GatttTimeout = time.Duration(5 * time.Second)
+
+const (
+	GatttWriteSuccess     = "Characteristic value was written successfully"
+	GatttNotificationRead = "Notification handle"
+)
+
+func TimedGatttExec(cmd *exec.Cmd, timeout time.Duration, waitFor string) ([]string, error) {
+	done := make(chan struct{})
+
+	var result []string
+
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		return result, err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return result, err
+	}
+
+	scanner := bufio.NewScanner(stdOut)
+	go func() {
+		for scanner.Scan() {
+			text := scanner.Text()
+			result = append(result, text)
+
+			if strings.Contains(text, waitFor) {
+				log.Infof("Stopping gatttool upon catching target string %s", text)
+				cmd.Process.Kill()
+				close(done)
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-time.After(3 * time.Second):
+		log.Infof("Stopping gatttool upon timeout")
+		cmd.Process.Kill()
+		return result, GatttTimeoutError{}
+
+	case <-done:
+		return result, nil
+	}
+
+	return result, nil
+}
+
+type GatttTimeoutError struct{}
+
+func (GatttTimeoutError) Error() string {
+	return fmt.Sprintf("Timeout reached while waiting for gatttool output")
+}
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	log "github.com/romana/rlog"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there")
+}
+
+func getSecondOp(command, firstOp, secondOp string) string {
+	if command == "write" && firstOp == "curr-time" {
+		t := time.Now().Unix()
+		log.Infof("Writing current time %d -> %x", t, t)
+		return fmt.Sprintf("%x", t)
+	}
+
+	return secondOp
+}
+
+func timeFromResult(lines []string) (int64, error) {
+	if len(lines) != 2 {
+		return 0, fmt.Errorf("not enough lines in result, expect exactly 2 lines")
+	}
+
+	lastLineArr := strings.Split(lines[1], ":")
+	if len(lastLineArr) != 2 {
+		return 0, fmt.Errorf("failed to parse time out of line %v", lines)
+	}
+
+	return strconv.ParseInt(strings.Replace(lastLineArr[1], " ", "", -1), 16, 64)
+}
+
+func main() {
+	btInterface := flag.String("interface", "hci0", "Bluetooth interface to use)")
+	btHandle := flag.String("handle", "0x0025", "Have no idea what it is")
+	gtBin := flag.String("gtBin", "/usr/local/bin/gatttool", "Full path to gatttool")
+	targetMac := flag.String("mac", "00:15:83:40:72:75", "Mac address of the target node")
+	command := flag.String("command", "", "One of read|write|save|reset")
+	firstOp := flag.String("op1", "",
+		fmt.Sprintf("Operand for read|write commands\n\t%s", getMapKeys(OpCodes)))
+	secondOp := flag.String("op2", "", "Value for write command (auto for write curr-time)")
+
+	flag.Parse()
+
+	g := Gattt{
+		Interface:  *btInterface,
+		Handle:     *btHandle,
+		Mac:        *targetMac,
+		GatttolBin: *gtBin,
+	}
+
+	if cmd, ok := CommandCodes[*command]; !ok {
+		log.Infof("Unkonwn command %s", *command)
+		flag.Usage()
+		os.Exit(1)
+	} else {
+		g.Command = cmd
+	}
+
+	if opc, ok := OpCodes[*firstOp]; !ok {
+		if *command == "write" || *command == "read" {
+			log.Infof("Unkonwn operand %s", *firstOp)
+			flag.Usage()
+			os.Exit(1)
+		}
+
+		g.Op1 = "00"
+	} else {
+		g.Op1 = opc
+	}
+
+	g.Length = OpLength[*firstOp]
+	g.Op2 = getSecondOp(*command, *firstOp, *secondOp)
+
+	var result []string
+	var err error
+
+	switch *command {
+	case "read":
+		result, err = g.Read()
+	case "write":
+		err = g.Write()
+	case "save":
+		err = g.Save()
+	case "reset":
+		err = g.Reset()
+	}
+	if err != nil {
+		log.Infof("Error %s", err.Error())
+		os.Exit(2)
+	}
+
+	if *command == "read" && strings.Contains(*firstOp, "time") {
+		readTime, err := timeFromResult(result)
+		if err != nil {
+			log.Errorf("Failed to detect time marker in %s", result)
+		}
+		fmt.Printf("Time received %d\n", readTime)
+	}
+	log.Infof("Result %v", result)
+}
